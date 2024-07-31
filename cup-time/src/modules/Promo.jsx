3e20ef612568bd8ac87c7c151f0026966c5a8fa3
arqu33n <soondawn@yandex.ru>
2024-08-01 01:24:57 +0300

    feat: add react app

---
+++ cup-time/src/modules/Promo.jsx
@@ -0,0 +1,12 @@
+export const Promo = () => (
+  <section class="promo">
+    <div class="container">
+      <div class="promo__container">
+        <h1 class="promo__title">Попробуй новый вкус Арабики</h1>
+        <a href="#" class="promo__link">
+          Перейти к кофе
+        </a>
+      </div>
+    </div>
+  </section>
+);
